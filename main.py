71046f0c4a6deb0f0a2ccde69a503ffaadeba886
adiamlani130 <aamlani130@gmail.com>
2024-09-23 12:27:38 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
 class main:
+  print("Welcome To security Detection")
+  phoneNumber=input("Enter your phone number to recieve threat alerts: ")
+  print("Click the arm button to activate system")
+  
