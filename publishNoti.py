17a0dcbac75085fbccf9a1a0d097ac032806bca8
adiamlani130 <aamlani130@gmail.com>
2024-09-29 22:20:41 -0400

    Update publishNoti.py

--- publishNoti.py
+++ publishNoti.py
@@ -1,32 +1,28 @@
 import boto3
+#remember to configure aws, pip install boto3, add the info
+def send_sms(phone_number, message):
+    """
+    Send a message to a phone number using AWS SNS.
+    
+    :param phone_number: The recipient's phone number in E.164 format (e.g., +1234567890)
+    :param message: The message content to send
+    """
+    # Create an SNS client
+    sns = boto3.client('sns')
 
-topic_arn = ""
-def send_sns(message, subject):
     try:
-        client = boto3.client("sns")
-        result = client.publish(TopicArn=topic_arn, Message=message, Subject=subject)
-        if result['ResponseMetadata']['HTTPStatusCode'] == 200:
-            print(result)
-            print("Notification send successfully..!!!")
-            return True
+        # Send the message
+        response = sns.publish(
+            PhoneNumber=phone_number,
+            Message=message
+        )
+        print(f"Message sent! Message ID: {response['MessageId']}")
     except Exception as e:
-        print("Error occured while publish notifications and error is : ", e)
-        return True
+        print(f"Error sending message: {e}")
 
-def lambda_handler(event, context):
-    print("event collected is {}".format(event))
-    for record in event['Records'] :
-        s3_bucket = record['s3']['bucket']['name']
-        print("Bucket name is {}".format(s3_bucket))
-        s3_key = record['s3']['object']['key']
-        print("Bucket key name is {}".format(s3_key))
-        from_path = "s3://{}/{}".format(s3_bucket, s3_key)
-        print("from path {}".format(from_path))
-        message = "The file is uploaded at S3 bucket path {}".format(from_path)
-        subject = "Processes completion Notification"
-        SNSResult = send_sns(message, subject)
-        if SNSResult :
-            print("Notification Sent..") 
-            return SNSResult
-        else:
-            return False
+# Example usage
+if __name__ == "__main__":
+    phone_number = "+1234567890"  # Replace with your phone number
+    message = "This is a test message from AWS SNS"
+    
+    send_sms(phone_number, message)
