7a62a847b570b36d8617bdbb13a9b8bbdb40d3c5
adiamlani130 <aamlani130@gmail.com>
2024-10-01 11:08:05 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import tkinter as tk
 class main:
+  armed = false
   print("Welcome To security Detection")
   phoneNumber=input("Enter your phone number to recieve threat alerts: ")
-  print("Click the arm button to activate system")
-  
+  root = tk.Tk()
+  # Create a button
+  button = tk.Button(window, text="Arm", command=lambda: print("Button clicked!"))
+  button.pack(pady=20)
+  # Run the main loop
+  window.mainloop()
+  def button_clicked():
+    print("System Armed: "+ armed)
+    armed= not armed
+    #run with conditional
+
+      
